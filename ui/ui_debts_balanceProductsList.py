cf4e72f362e34b89e9e4e2d0b6bda8c9af1f13a2
Bruno Heit <Brunoh19@hotmail.com>
2025-03-17 17:47:22 -0300

    ADDED button to delete registers in ProductsBalanceDialog

--- ui/ui_debts_balanceProductsList.py
+++ ui/ui_debts_balanceProductsList.py
@@ -17,20 +17,20 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractItemView, QAbstractScrollArea, QApplication, QCheckBox,
     QDialog, QFrame, QHBoxLayout, QHeaderView,
-    QLineEdit, QSizePolicy, QTableView, QVBoxLayout,
-    QWidget)
+    QLineEdit, QPushButton, QSizePolicy, QTableView,
+    QVBoxLayout, QWidget)
 
 class Ui_ProductsBalance(object):
     def setupUi(self, ProductsBalance):
         if not ProductsBalance.objectName():
             ProductsBalance.setObjectName(u"ProductsBalance")
-        ProductsBalance.resize(400, 150)
+        ProductsBalance.resize(400, 160)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(ProductsBalance.sizePolicy().hasHeightForWidth())
         ProductsBalance.setSizePolicy(sizePolicy)
-        ProductsBalance.setMinimumSize(QSize(400, 100))
+        ProductsBalance.setMinimumSize(QSize(400, 160))
         ProductsBalance.setMaximumSize(QSize(400, 16777215))
         ProductsBalance.setWindowOpacity(1.000000000000000)
         ProductsBalance.setStyleSheet(u"* {\n"
@@ -60,6 +60,25 @@ class Ui_ProductsBalance(object):
 "}\n"
 "\n"
 "\n"
+"/* pushbutton */\n"
+"QPushButton {\n"
+"	color: #fff;\n"
+"	background-color: #415a77;\n"
+"	border: None;\n"
+"	border-radius: 4px;\n"
+"}\n"
+"QPushButton:hover,\n"
+"QPushButton:pressed {\n"
+"	background-color: #faa;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	border: 1px inset #778da9;\n"
+"}\n"
+"QPushButton:disabled {\n"
+"	color: #999;\n"
+"}\n"
+"\n"
+"\n"
 "/* tableview y headers */\n"
 "QTableView {\n"
 "	color: #0d1b2a;\n"
@@ -71,7 +90,8 @@ class Ui_ProductsBalance(object):
 "	background-color: #778da9;\n"
 "}\n"
 "QTableView::item:selected {\n"
-"	background-color: #778da9;\n"
+"	background-col"
+                        "or: #778da9;\n"
 "	border: 1px solid #fff;\n"
 "}\n"
 "/* QHeaderViews */\n"
@@ -88,8 +108,7 @@ class Ui_ProductsBalance(object):
 "/* lineedits */\n"
 "QLineEdit {\n"
 "	background-color: #e0e1dd;\n"
-"	col"
-                        "or: #0d1b2a;\n"
+"	color: #0d1b2a;\n"
 "	border: none;\n"
 "	border-radius: 10px;\n"
 "	padding-left: 3px;\n"
@@ -121,7 +140,8 @@ class Ui_ProductsBalance(object):
 "\n"
 "\n"
 "/* scrollbars */\n"
-"QScrollBar {\n"
+""
+                        "QScrollBar {\n"
 "	background-color: #e0e1dd;\n"
 "	border: 1px solid transparent;\n"
 "	border-radius: 5px;\n"
@@ -136,8 +156,7 @@ class Ui_ProductsBalance(object):
 "QScrollBar::handle:pressed {\n"
 "	background-color: #3b465b;\n"
 "}\n"
-"QScrollBar::s"
-                        "ub-line {\n"
+"QScrollBar::sub-line {\n"
 "	width: 0;\n"
 "	height: 0;\n"
 "	background: none;\n"
@@ -170,27 +189,32 @@ class Ui_ProductsBalance(object):
 "QScrollBar::handle:horizontal {\n"
 "	min-width: 15px;\n"
 "}")
-        self.verticalLayout = QVBoxLayout(ProductsBalance)
-        self.verticalLayout.setSpacing(0)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_4 = QHBoxLayout(ProductsBalance)
+        self.horizontalLayout_4.setSpacing(0)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
         self.central_widget = QWidget(ProductsBalance)
         self.central_widget.setObjectName(u"central_widget")
+        self.central_widget.setMinimumSize(QSize(0, 160))
         self.verticalLayout_2 = QVBoxLayout(self.central_widget)
         self.verticalLayout_2.setSpacing(8)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(5, 5, 5, 5)
         self.header = QWidget(self.central_widget)
         self.header.setObjectName(u"header")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.header.sizePolicy().hasHeightForWidth())
+        self.header.setSizePolicy(sizePolicy1)
+        self.header.setMinimumSize(QSize(0, 24))
+        self.header.setMaximumSize(QSize(16777215, 24))
         self.horizontalLayout = QHBoxLayout(self.header)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(30, 0, 30, 0)
         self.search_bar = QLineEdit(self.header)
         self.search_bar.setObjectName(u"search_bar")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.search_bar.sizePolicy().hasHeightForWidth())
         self.search_bar.setSizePolicy(sizePolicy1)
         self.search_bar.setMinimumSize(QSize(0, 24))
@@ -206,6 +230,7 @@ class Ui_ProductsBalance(object):
 
         self.body = QWidget(self.central_widget)
         self.body.setObjectName(u"body")
+        self.body.setMinimumSize(QSize(0, 86))
         self.verticalLayout_3 = QVBoxLayout(self.body)
         self.verticalLayout_3.setSpacing(5)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
@@ -233,7 +258,17 @@ class Ui_ProductsBalance(object):
 
         self.verticalLayout_3.addWidget(self.tv_balance_products)
 
-        self.checkbox_show_all_products = QCheckBox(self.body)
+        self.lower_body = QWidget(self.body)
+        self.lower_body.setObjectName(u"lower_body")
+        sizePolicy1.setHeightForWidth(self.lower_body.sizePolicy().hasHeightForWidth())
+        self.lower_body.setSizePolicy(sizePolicy1)
+        self.lower_body.setMinimumSize(QSize(0, 24))
+        self.lower_body.setMaximumSize(QSize(16777215, 24))
+        self.horizontalLayout_3 = QHBoxLayout(self.lower_body)
+        self.horizontalLayout_3.setSpacing(4)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.checkbox_show_all_products = QCheckBox(self.lower_body)
         self.checkbox_show_all_products.setObjectName(u"checkbox_show_all_products")
         sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Fixed)
         sizePolicy3.setHorizontalStretch(0)
@@ -247,24 +282,46 @@ class Ui_ProductsBalance(object):
 #endif // QT_CONFIG(tooltip)
         self.checkbox_show_all_products.setIconSize(QSize(24, 24))
 
-        self.verticalLayout_3.addWidget(self.checkbox_show_all_products)
+        self.horizontalLayout_3.addWidget(self.checkbox_show_all_products)
+
+        self.btn_delete_debt = QPushButton(self.lower_body)
+        self.btn_delete_debt.setObjectName(u"btn_delete_debt")
+        self.btn_delete_debt.setEnabled(False)
+        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.btn_delete_debt.sizePolicy().hasHeightForWidth())
+        self.btn_delete_debt.setSizePolicy(sizePolicy4)
+        self.btn_delete_debt.setMinimumSize(QSize(24, 24))
+        self.btn_delete_debt.setMaximumSize(QSize(24, 24))
+        self.btn_delete_debt.setIconSize(QSize(24, 24))
+        self.btn_delete_debt.setFlat(False)
+
+        self.horizontalLayout_3.addWidget(self.btn_delete_debt, 0, Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignVCenter)
+
+
+        self.verticalLayout_3.addWidget(self.lower_body)
 
 
         self.verticalLayout_2.addWidget(self.body)
 
         self.footer = QWidget(self.central_widget)
         self.footer.setObjectName(u"footer")
+        sizePolicy1.setHeightForWidth(self.footer.sizePolicy().hasHeightForWidth())
+        self.footer.setSizePolicy(sizePolicy1)
+        self.footer.setMinimumSize(QSize(0, 24))
+        self.footer.setMaximumSize(QSize(16777215, 24))
         self.horizontalLayout_2 = QHBoxLayout(self.footer)
         self.horizontalLayout_2.setSpacing(0)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.horizontalLayout_2.setContentsMargins(15, 0, 15, 0)
         self.le_reduce_debt = QLineEdit(self.footer)
         self.le_reduce_debt.setObjectName(u"le_reduce_debt")
-        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Fixed)
-        sizePolicy4.setHorizontalStretch(0)
-        sizePolicy4.setVerticalStretch(0)
-        sizePolicy4.setHeightForWidth(self.le_reduce_debt.sizePolicy().hasHeightForWidth())
-        self.le_reduce_debt.setSizePolicy(sizePolicy4)
+        sizePolicy5 = QSizePolicy(QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Fixed)
+        sizePolicy5.setHorizontalStretch(0)
+        sizePolicy5.setVerticalStretch(0)
+        sizePolicy5.setHeightForWidth(self.le_reduce_debt.sizePolicy().hasHeightForWidth())
+        self.le_reduce_debt.setSizePolicy(sizePolicy5)
         self.le_reduce_debt.setMinimumSize(QSize(0, 24))
         self.le_reduce_debt.setMaximumSize(QSize(16777215, 24))
 #if QT_CONFIG(tooltip)
@@ -279,7 +336,7 @@ class Ui_ProductsBalance(object):
         self.verticalLayout_2.addWidget(self.footer)
 
 
-        self.verticalLayout.addWidget(self.central_widget)
+        self.horizontalLayout_4.addWidget(self.central_widget)
 
 
         self.retranslateUi(ProductsBalance)
@@ -297,6 +354,10 @@ class Ui_ProductsBalance(object):
 #endif // QT_CONFIG(tooltip)
         self.search_bar.setPlaceholderText(QCoreApplication.translate("ProductsBalance", u"Buscar ventas por fecha, cantidad, etc...", None))
         self.checkbox_show_all_products.setText(QCoreApplication.translate("ProductsBalance", u"Mostrar historial de productos", None))
+#if QT_CONFIG(tooltip)
+        self.btn_delete_debt.setToolTip(QCoreApplication.translate("ProductsBalance", u"<html><head/><body><p><span style=\" font-size:11pt;\">Elimina la cantidad adeudada/a favor actualmente seleccionada en la tabla.</span></p><p><span style=\" font-size:11pt; font-weight:600; text-decoration: underline;\">IMPORTANTE</span><span style=\" font-size:11pt; font-weight:600;\">: esta acci\u00f3n no se puede deshacer, se debe estar seguro de querer borrar un registro.</span></p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
+        self.btn_delete_debt.setText("")
         self.le_reduce_debt.setPlaceholderText(QCoreApplication.translate("ProductsBalance", u"Descontar del saldo de productos seleccionados...", None))
     # retranslateUi
 
